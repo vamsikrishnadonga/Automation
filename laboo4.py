b00507b8ee8bd96e492a00c8d0a0c0caa2242522
vamsikrishnadonga <147294171+vamsikrishnadonga@users.noreply.github.com>
2023-10-08 10:16:55 +0530

    Add files via upload

---
+++ laboo4.py
@@ -0,0 +1,9 @@
+name=input("Enter Your Name \n")
+print(name)
+
+First_name=input("Enter your First name:\n")
+Last_name=input("Enter your Last name:\n")
+print(First_name,Last_name,sep="_",end="\n")
+
+name1=input("Enter Your Name\n")
+print(name1,"You are welcome to the python class")
